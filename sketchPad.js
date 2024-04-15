55f72e80c68cabc8782209b76c9020e9bd77d3ee
Sanjay P <83605663+alphaSanjay@users.noreply.github.com>
2024-04-15 19:49:59 +0530

    Added Reset feature
    resetGrid() function is added to js
    reset button style is added in css

--- sketchPad.js
+++ sketchPad.js
@@ -1,20 +1,48 @@
 const container = document.querySelector(".container")
-let rows = document.querySelector(".gridRow")
-num = 20
-makeGrid(num)
+let id = 0
+let boxHover = ' '
 
+makeGrid(16)
+
+function resetGrid(){
+    num = prompt("Please enter the Size: ")
+    while(num > 100 || num <= 0){
+        alert("Please enter between 1 - 100")
+        num = prompt("Please enter the Size: ")
+    }
+    console.log(2)
+    while(container.firstChild) container.removeChild( container.lastChild )
+    makeGrid(num)
+} 
 
 function makeGrid(size){
-    let height = 600 / num
+    console.log(1)
+    let height = 600 / size
     let ht = height + 'px'
     for(i = 0; i < size; i++){
         let row = document.createElement("div")
         row.style.height = ht
         container.appendChild(row).className = "gridRow"
         for(j = 0; j < size; j++){
+            id++
             let column = document.createElement("div")
             row.appendChild(column).className = "cell"
             column.style.flex = 1
+            column.setAttribute('id', 'b'+id)
         }
+    }
+    let jin = id
+    id = 0
+    applyColor(jin)
+}
+
+function applyColor(jin){
+    console.log(3)
+    for (i = 1; i <= jin; i++) {
+        selector = '#' + 'b'+ i;
+        boxHover = document.querySelector(selector);
+        boxHover.addEventListener('mouseover', (event) => {
+            event.target.style.backgroundColor = 'blue';
+        })
     }
 }
